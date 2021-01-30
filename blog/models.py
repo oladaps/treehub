452511e4d02ff7e8e1ec0b35c47ddbb895dc3361
odus <oduoyesubset@gmail.com>
2021-01-31 00:57:33 +0100

    create class Article in model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(db_index=True)
+    description = models.CharField(max_length=500)
+    
+    
+    
+    def __str__(self):
+        return self.title
